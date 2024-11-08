53961d38d7ea804d7e15c92b58d67f5697ca0346
priyanshu3301 <165797837+priyanshu3301@users.noreply.github.com>
2024-11-08 23:45:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,3 @@ def predict():
     prediction = pipe.predict(test)[0]
     return str(int(prediction))
     
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
